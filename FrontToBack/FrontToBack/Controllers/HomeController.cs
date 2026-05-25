9158d379fdce4f879d38b22973afd005b33a4ba1
narminhasanova5 <narahasan12@gmail.com>
2026-05-25 13:33:15 +0400

    first commit

---
+++ FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿
+using FrontToBack.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Services = await _context.Services.ToListAsync();
+            return View(Services);
+        }
+
+       
+    }
+}
---
+++ FrontToBack/FrontToBack/Models/Services.cs
@@ -0,0 +1,9 @@
+﻿namespace FrontToBack.Models
+{
+    public class Services :BaseEntity
+    {
+        public string Images { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
