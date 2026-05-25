2ee50d2b7a192d7d45c8b2342614ed384c1ada67
safarovyusif <safarovyusif@yahoo.com>
2026-05-26 00:03:19 +0400

    VM

---
+++ FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using FrontToBack.Data;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            var services = await _context.Services.ToListAsync();
+
+           
+            HomeVM homeVM = new HomeVM
+            {
+                Services = services
+            };
+
+           
+            return View(homeVM);
+        }
+    }
+}
---
+++ FrontToBack/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using FrontToBack.Models;
+using System.Collections.Generic;
+
+namespace FrontToBack.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Services> Services { get; set; }
+
+        
+    }
+}
